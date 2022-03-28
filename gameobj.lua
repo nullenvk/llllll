63728467f3ee334ec0d973d1633032ed489d9c21
nullenvk <nullenvk@protonmail.com>
2022-03-28 16:09:36 +0200

    gameobj system

---
+++ gameobj.lua
@@ -0,0 +1,30 @@
+GAMEOBJ_STATUS_OK = 0
+GAMEOBJ_STATUS_GARBAGE = -1
+
+GameObj = { runStatus = GAMEOBJ_STATUS_OK}
+
+function GameObj:new(o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
+function GameObj:runUpdate(dt)
+    if self.runStatus == GAMEOBJ_STATUS_OK then
+        self:update(dt)
+    end
+end
+
+function GameObj:runDraw()
+    if self.runStatus == GAMEOBJ_STATUS_OK then
+        self:draw()
+    end
+end
+
+function GameObj:draw()
+end
+
+function GameObj:update()
+    error("update method not defined for a game object")
+end
--- main.lua
+++ main.lua
@@ -1,7 +1,42 @@
-function love.load()
+require('gameobj')
 
+Player = GameObj:new({timer = 0})
+
+function Player:new(o)
+    o = o or GameObj:new(o)
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
+function Player:update(dt)
+    self.timer = self.timer + dt
+
+    if self.timer > 5.0 then self.runStatus = GAMEOBJ_STATUS_GARBAGE end
+end
+
+function Player:draw()
+    love.graphics.rectangle("fill", 64, 64, 64, 64)
+end
+
+sceneObjs = {}
+
+function love.load()
+    table.insert(sceneObjs, Player:new())
+end
+
+function love.update(dt)
+    for k,v in pairs(sceneObjs) do 
+        v:runUpdate(dt)
+        
+        if v.runStatus == GAMEOBJ_STATUS_GARBAGE then
+            table.remove(sceneObjs, k)
+        end
+    end
+
+    print(#sceneObjs)
 end
 
 function love.draw()
-
+    for _,v in pairs(sceneObjs) do v:runDraw() end
 end
