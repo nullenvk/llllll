268af96b4f4dc9fe2c71df3a8a7ff11dbf493a99
nullenvk <nullenvk@protonmail.com>
2022-04-09 15:09:45 +0200

    screen switching

--- state_game.lua
+++ state_game.lua
@@ -7,9 +7,8 @@ State_Game = GameState:new({})
 
 local LAYERNUM_BACKGROUND = 0
 local LAYERNUM_MAP = 1
-local LAYERNUM_ENTS = 2
 
-local LAYERNUM_NUM = 4
+local LAYERNUM_NUM = 2
 
 local curScene = {}
 local tmap = nil
@@ -51,19 +50,33 @@ function ObjTiles:update(_)
 
 end
 
-function State_Game:init()
+function State_Game:switchScreen(dx, dy)
     curScene = {}
     for _=1,LAYERNUM_NUM do table.insert(curScene, {}) end
 
-    tmap = TileMap:new(nil, "res/main.map")
-    screenPos = {x = 1, y = 1}
+    screenPos.x = screenPos.x + dx
+    screenPos.y = screenPos.y + dy
 
-    Player.preload()
-    playerObj = Player:new()
-    playerObj.pos = {x = 400, y = 300}
-    curScene[LAYERNUM_ENTS]["player"] = playerObj
+    print(screenPos.x, screenPos.y)
 
+    if playerObj == nil then
+        playerObj = Player:new()
+        playerObj.pos = {x = 400, y = 300}
+    else
+        if dx ~= 0 then playerObj.pos.x = 800 - playerObj.pos.x end
+        if dy ~= 0 then playerObj.pos.y = 600 - playerObj.pos.y end
+    end
+
+    curScene[LAYERNUM_MAP]["player"] = playerObj
     curScene[LAYERNUM_MAP]["tiles"] = ObjTiles:new(nil, screenPos.x, screenPos.y)
+end
+
+function State_Game:init()
+    tmap = TileMap:new(nil, "res/main.map")
+    Player.preload()
+
+    screenPos = {x = 1, y = 1}
+    self:switchScreen(0,0)
 end
 
 function State_Game:draw()
@@ -75,6 +88,17 @@ function State_Game:draw()
         for _,v in pairs(sceneObjs) do v:runDraw() end
     end
 
+end
+
+function State_Game:testScrSwitch()
+    -- TODO: ugly code, change later
+    local dx, dy = 0, 0
+    if playerObj.pos.x > 800 then dx = 1 end
+    if playerObj.pos.x < 0 then dx = -1 end
+    if playerObj.pos.y > 600 then dy = 1 end
+    if playerObj.pos.y < 0 then dy = -1 end
+
+    if dx ~= 0 or dy ~= 0 then self:switchScreen(dx, dy) end
 end
 
 function State_Game:update(dt)
@@ -101,6 +125,8 @@ function State_Game:update(dt)
     -- Inputs
     if love.keyboard.isDown("escape") then newState = State_MainMenu end
 
+    self:testScrSwitch()
+
     return newState
 end
 
@@ -118,7 +144,7 @@ function State_Game:fini()
 end
 
 function GameState:keypressed(key, _, isrepeat)
-    local player = curScene[LAYERNUM_ENTS]["player"]
+    local player = curScene[LAYERNUM_MAP]["player"]
     if player == nil then return end
 
     if key == "space" and not isrepeat then
